e356af1a8ffef2d4b296689c613a71d2a46bc24f
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-04-20 13:10:03 +0530

    redis.sh

--- payment.sh
+++ payment.sh
@@ -5,7 +5,7 @@ echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 
 echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd ${app_user}
+useradd roboshop
 
 echo -e "\e[36m>>>>>>>>> Create App Dir <<<<<<<<\e[0m"
 rm -rf /app
@@ -22,7 +22,7 @@ echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>>Setup SystemD Service <<<<<<<<\e[0m"
-cp ${script_path}/payment.service /etc/systemd/system/payment.service
+cp roboshop/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>> Start Payment Service <<<<<<<<\e[0m"
 systemctl daemon-reload
