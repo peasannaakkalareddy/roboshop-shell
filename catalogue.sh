49e44ce97a99891d8523591999cdb75bf84d7162
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-01 17:18:55 +0530

    learn-shell

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 #echo $script_path
 #exit
-source $script_path/common.sh
+source ${script_path}/common.sh
 component=catalogue
 func_nodejs
 
