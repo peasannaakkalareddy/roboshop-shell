56df30e0c8d282a6ede045e154d13322afd54d32
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-03 22:54:12 +0530

    learn-terraform

--- frontend.sh
+++ frontend.sh
@@ -3,22 +3,22 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head " Install Nginx server "
-yum install nginx -y &>>${log_file}
+yum install nginx -y &>>$logfile
 func_status_check $?
 func_print_head " Copy roboshop configuration file "
-cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$logfile
 func_status_check $?
 func_print_head " Remove old app content "
-rm -rf /usr/share/nginx/html/* &>>$log_file
+rm -rf /usr/share/nginx/html/* &>>$logfile
 func_status_check $?
 func_print_head "  Download App Content "
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$logfile
 func_status_check $?
-cd /usr/share/nginx/html &>>$log_file
+cd /usr/share/nginx/html &>>$logfile
 func_print_head " Unzip App Content "
-unzip /tmp/frontend.zip &>>$log_file
+unzip /tmp/frontend.zip &>>$logfile
 func_status_check $?
 func_print_head " Start nginx Service "
-systemctl restart nginx &>>$log_file
-systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$logfile
+systemctl enable nginx &>>$logfile
 func_status_check $?
