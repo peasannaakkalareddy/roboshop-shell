55e4a63c6d6e40d7450ec5f681ced62ded74ed5b
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-25 23:21:09 +0530

    roboshop - ansible

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,13 +10,17 @@ fi
 echo -e "\e[36m>>>>>>>>> Setup ErLang Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>${logfile}
 func_status_check $?
+echo -e "\e[36m>>>>>>>>> Install ErLang & RabbitMQ <<<<<<<<\e[0m"
+yum install erlang -y &>${logfile}
+func_status_check $?
+
 
 echo -e "\e[36m>>>>>>>>> Setup RabbitMQ Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>${logfile}
 func_status_check $?
 
 echo -e "\e[36m>>>>>>>>> Install ErLang & RabbitMQ <<<<<<<<\e[0m"
-yum install erlang rabbitmq-server -y &>${logfile}
+yum install rabbitmq-server -y &>${logfile}
 func_status_check $?
 
 echo -e "\e[36m>>>>>>>>> Start RabbitMQ Service <<<<<<<<\e[0m"
